fba52b49c2f5dd9b15f875bb9995aadcf735e036
Andreas Zaschka <myemailfach@gmail.com>
2016-06-13 18:46:16 +0200

    test with simple echo bot

---
+++ cmd/timer/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	http.HandleFunc("/", handler)
+    http.ListenAndServe(":" + port, nil)
+}
