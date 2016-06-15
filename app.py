bd25eb69e640846addf7eb2c901bb8090c0dcf91
Andreas Zaschka <myemailfach@gmail.com>
2016-06-15 18:16:26 +0200

    added scheduler

--- app.py
+++ app.py
@@ -3,23 +3,32 @@ import requests
 import logging
 import sys
 import json
+import sched
+import time
 
 from flask import Flask
 
 app = Flask(__name__)
 
 log = logging.getLogger('TimerBot')
+scheduler = sched.scheduler(time.time, time.sleep)
 
 @app.route('/timer/<int:minutes>/<string:room_id>/<string:token>', methods=['GET', 'POST'])
 def create_timer(minutes, room_id, token):
 
 	log.info('create timer with %d minutes for room %s with token %s', minutes, room_id, token)
 
+	notify_room(room_id, token, 'Timer gestartet ...')
+
+	return 'created timer with %d minutes' % minutes
+
+def notify_room(room_id, token, message):
+
 	url = 'https://bindoc.hipchat.com/v2/room/' + room_id + '/notification?auth_token=' + token
 
 	headers = {'Content-type': 'application/json'}
 
-	payload = {'color': 'green','message': 'Timer gestartet ...','notify': True,'message_format': 'text'}
+	payload = {'color': 'green','message': message,'notify': True,'message_format': 'text'}
 
 	r = requests.post(url, headers = headers, data = json.dumps(payload))
 
@@ -29,7 +38,12 @@ def create_timer(minutes, room_id, token):
 		log.info('url= %s', r.url)
 		log.info('content= %s', r.content)
 
-	return 'created timer with %d' % minutes
+def set_scheduler(minutes, room_id, token):
+
+	scheduler.enter(60*minutes, 1, notify_room, (room_id, token, 'Timer abgelaufen'))
+
+	scheduler.run()
+
 
 if __name__ == '__main__':
 
