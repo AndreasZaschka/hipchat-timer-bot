b19d3e43599ac7e4b51c7708e618045e303212ba
Andreas Zaschka <myemailfach@gmail.com>
2016-06-15 16:18:32 +0200

    add missing files

---
+++ app.py
@@ -0,0 +1,26 @@
+import os
+import requests
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/timer/<int:minutes>/<string:room_id>/<string:token>', methods=['POST'])
+def createTimer(minutes, room_id, token):
+
+	url = 'https://bindoc.hipchat.com/v2/room/' + room_id + '/notification?auth_token=' + token
+
+	r = requests.post(url, data = {
+  				'color': 'green', 
+  				'message': 'Timer gestartet ...', 
+  				'notify': true, 
+  				'message_format': 'text'
+				})
+
+    return 'Creating timer with %d' % minutes
+
+if __name__ == '__main__':
+
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
