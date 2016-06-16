581141b145e64fc45562ba8ba3426f7705be75ff
Andreas Zaschka <myemailfach@gmail.com>
2016-06-16 12:57:49 +0200

    new command syntax

--- app.py
+++ app.py
@@ -8,36 +8,40 @@ import time
 from threading import Timer
 from flask import Flask
 from flask import request
+from command import HTBCommand
 
 app = Flask(__name__)
 
 log = logging.getLogger('TimerBot')
 
-token = None
+session = {}
 
 @app.route('/timer', methods=['GET', 'POST'])
 def create_timer():
 
 	rData = request.get_json(silent=True)
-	log.info('rData: %s', rData)
 
 	message = rData['item']['message']['message']
-	log.info('message = %s', message)
 
 	room_id = rData['item']['room']['id']
-	log.info('room_id = %s', room_id)
 
-	#log.info('create timer with %d minutes for room %s with token %s', minutes, room_id, token)
+	cmd = HTBCommand(message)
 
-	#notify_room(room_id, token, 'Timer gestartet ...')
+	if (cmd.command == 'CONFIG'):
+		register_token(room_id, cmd.name)
+		notify_room(room_id, 'Token gespeichert :D')
 
-	#set_scheduler(minutes, room_id, token)
+	if (cmd.command == 'NEW'):
+		start_timer(room_id, cmd)
 
 	return 'created timer with 10 minutes'
 
-def notify_room(room_id, token, message):
+def notify_room(room_id, message):
 
-	url = 'https://bindoc.hipchat.com/v2/room/' + room_id + '/notification?auth_token=' + token
+	if (room_id not in session):
+		return
+
+	url = 'https://bindoc.hipchat.com/v2/room/' + room_id + '/notification?auth_token=' + session[room_id]
 
 	headers = {'Content-type': 'application/json'}
 
@@ -51,9 +55,31 @@ def notify_room(room_id, token, message):
 		log.info('url= %s', r.url)
 		log.info('content= %s', r.content)
 
-def set_scheduler(minutes, room_id, token):
+def set_scheduler(cmd, room_id, token):
 
-	Timer(60 * minutes, notify_room, (room_id, token, 'Timer abgelaufen')).start()
+	message_str = '%s mit %d min abgelaufen' % (cmd.name, cmd.minutes)
+
+	Timer(60 * cmd.minutes, notify_room, (room_id, message_str)).start()
+
+def start_timer(room_id, cmd):
+
+	if (room_id not in session): return
+
+	if (cmd.minutes == None): return
+
+	if (cmd.name == None): return
+
+	message_str = '%s mit %d min gestartet ...' % (cmd.name, cmd.minutes)
+
+	notify_room(room_id, message_str)
+
+	set_scheduler(cmd, room_id, session[room_id])
+
+
+def register_token(room_id, token):
+	if (room_id not in session):
+		item = {room_id : token}
+		session.update(item)
 
 if __name__ == '__main__':
 
